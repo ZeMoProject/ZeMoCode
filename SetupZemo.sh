1c071927402e3053bd2ee715f8c3f5107e5345ca
Jonathon Hill <jhill@byu.edu>
2018-03-19 11:22:39 -0600

    Added install script

---
+++ SetupZemo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+cd ~
+
+#Move .bashrc for autostart
+cp ~/Zemo/.bashrc ~
+
+#Install monitor drivers
+wget https://raw.githubusercontent.com/adafruit/Adafruit-PiTFT-Helper/master/adafruit-pitft-helper2.sh
+chmod +x adafruit-pitft-helper2.sh
+printf "1\n3\ny\ny/n" | sudo ./adafruit-pitft-helper2.sh 
