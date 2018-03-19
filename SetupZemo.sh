8cde209e71eb6c7f75d6a4cf26f3679a83206a8f
Jonathon Hill <jhill@byu.edu>
2018-03-19 12:52:38 -0600

    move to profile

--- SetupZemo.sh
+++ SetupZemo.sh
@@ -3,7 +3,7 @@
 cd ~
 
 #Move .bashrc for autostart
-cp ~/ZeMoCode/bashrc ~/.bashrc
+cp ~/ZeMoCode/bashrc ~/.profile
 
 #Install monitor drivers
 wget https://raw.githubusercontent.com/adafruit/Adafruit-PiTFT-Helper/master/adafruit-pitft-helper2.sh
